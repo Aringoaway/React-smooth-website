2be5837e986462605c6891c2d3de092d5c3f6c35
Aringoaway <a.konshina.job@gmail.com>
2022-07-04 18:04:04 +0200

    add other svg

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -3,9 +3,9 @@ import {
 	ServicesContainer, ServicesH1, ServicesWrapper,
 	ServicesCard, ServicesIcon, ServicesH2, ServicesP
 } from "./ServicesElements"
-import Icon1 from "../../images/svg-1.svg";
-import Icon2 from "../../images/svg-2.svg";
-import Icon3 from "../../images/svg-3.svg";
+import Icon1 from "../../images/svg-4.svg";
+import Icon2 from "../../images/svg-5.svg";
+import Icon3 from "../../images/svg-6.svg";
 
 const Services = () => {
 	return (
